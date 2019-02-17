e29439f64cb3eadc0d4a2d845b59a027e78a7a71
Pavel Pesetskiy <pavel_pesetskiy@epam.com>
2019-02-17 16:07:28 +0300

    init

---
+++ WomanDayBot/WomanDayBotBot.cs
@@ -0,0 +1,132 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace WomanDayBot
+{
+    /// <summary>
+    /// Main entry point and orchestration for bot.
+    /// </summary>
+    public class WomanDayBotBot : IBot
+    {
+        // Supported LUIS Intents
+        public const string GreetingIntent = "Greeting";
+        public const string CancelIntent = "Cancel";
+        public const string HelpIntent = "Help";
+        public const string NoneIntent = "None";
+
+        /// <summary>
+        /// Key in the bot config (.bot file) for the LUIS instance.
+        /// In the .bot file, multiple instances of LUIS can be configured.
+        /// </summary>
+        public static readonly string LuisConfiguration = "MarchApp";
+
+        private readonly IStatePropertyAccessor<GreetingState> _greetingStateAccessor;
+        private readonly IStatePropertyAccessor<DialogState> _dialogStateAccessor;
+        private readonly ILogger<WomanDayBotBot> _logger;
+        private readonly UserState _userState;
+        private readonly ConversationState _conversationState;
+        private readonly BotServices _services;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CoreBot"/> class.
+        /// <param name="botServices">Bot services.</param>
+        /// <param name="accessors">Bot State Accessors.</param>
+        /// </summary>
+        public WomanDayBotBot(BotServices services, UserState userState, ConversationState conversationState, ILoggerFactory loggerFactory)
+        {
+            _services = services ?? throw new ArgumentNullException(nameof(services));
+            _userState = userState ?? throw new ArgumentNullException(nameof(userState));
+            _conversationState = conversationState ?? throw new ArgumentNullException(nameof(conversationState));
+
+            _greetingStateAccessor = _userState.CreateProperty<GreetingState>(nameof(GreetingState));
+            _dialogStateAccessor = _conversationState.CreateProperty<DialogState>(nameof(DialogState));
+            _logger = loggerFactory.CreateLogger<WomanDayBotBot>();
+
+            // Verify LUIS configuration.
+            if (!_services.LuisServices.ContainsKey(LuisConfiguration))
+            {
+                throw new InvalidOperationException($"The bot configuration does not contain a service type of `luis` with the id `{LuisConfiguration}`.");
+            }
+
+            Dialogs = new DialogSet(_dialogStateAccessor);
+            Dialogs.Add(new GreetingDialog(_greetingStateAccessor, loggerFactory));
+        }
+
+        private DialogSet Dialogs { get; set; }
+
+        /// <summary>
+        /// Run every turn of the conversation. Handles orchestration of messages.
+        /// <param name="turnContext">Bot Turn Context.</param>
+        /// <param name="cancellationToken">Task CancellationToken.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        /// </summary>
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            var activity = turnContext.Activity;
+
+            // Create a dialog context
+            var dc = await Dialogs.CreateContextAsync(turnContext);
+            var ff = activity.From.Name;
+            if (activity.Type == ActivityTypes.Message)
+            {
+               
+            }
+            else if (activity.Type == ActivityTypes.ConversationUpdate)
+            {
+                if (activity.MembersAdded != null)
+                {
+                    // Iterate over all new members added to the conversation.
+                    foreach (var member in activity.MembersAdded)
+                    {
+                        // Greet anyone that was not the target (recipient) of this message.
+                        // To learn more about Adaptive Cards,
+                        // See https://aka.ms/msbot-adaptivecards for more details.
+                        if (member.Id != activity.Recipient.Id)
+                        {
+                            var welcomeCard = CreateAdaptiveCardAttachment();
+                            var caurosel = MessageFactory.Carousel(new Attachment[] { welcomeCard, welcomeCard });
+                            //var response = CreateResponse(activity, caurosel);
+                            await dc.Context.SendActivityAsync(caurosel);
+                        }
+                    }
+                }
+            }
+
+            await _conversationState.SaveChangesAsync(turnContext);
+            await _userState.SaveChangesAsync(turnContext);
+        }
+
+        // Create an attachment message response.
+        private Activity CreateResponse(Activity activity, Attachment attachment)
+        {
+            var response = activity.CreateReply();
+            response.Attachments = new List<Attachment>() { attachment };
+            return response;
+        }
+
+        // Load attachment from file.
+        private Attachment CreateAdaptiveCardAttachment()
+        {
+            string[] paths = { ".", "Dialogs", "Welcome", "Resources", "welcomeCard.json" };
+            string fullPath = Path.Combine(paths);
+            var adaptiveCard = File.ReadAllText(fullPath);
+            return new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(adaptiveCard),
+            };
+        }
+    }
+}
