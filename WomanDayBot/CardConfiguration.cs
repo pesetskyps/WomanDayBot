536e46fbe831304492d1f172d5f5f1fc203ebd0e
Pavel Pesetskiy <pavel_pesetskiy@epam.com>
2019-02-23 19:08:18 +0300

    add dynamic cards config

---
+++ WomanDayBot/CardConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WomanDayBot
+{
+    public class CardConfiguration
+    {
+        public CardConfiguration(string ImageUrl, string TitleText, string Description, string TitleId)
+        {
+            this.TitleId = TitleId;
+            this.Description = Description;
+            this.ImageUrl = ImageUrl;
+            this.TitleText = TitleText;
+        }
+
+        public string ImageUrl { get; private set; }
+        public string TitleText { get; private set; }
+        public string Description { get; private set; }
+        public string TitleId { get; private set; }
+    }
+}
---
+++ WomanDayBot/CardConfigurationRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WomanDayBot
+{
+    public interface ICardConfigurationRepository
+    {
+        Task<List<CardConfiguration>> GetAsync();
+    }
+    public class CardConfigurationRepository : ICardConfigurationRepository
+    {
+        public async Task<List<CardConfiguration>> GetAsync()
+        {
+            return new List<CardConfiguration>
+            {
+                new CardConfiguration("https://picsum.photos/300?image=222","Bro","That's for Bro","bro"),
+                new CardConfiguration("https://picsum.photos/300?image=221","No Bro","That's for Not Bro","notbro")
+            };
+        }
+    }
+}
