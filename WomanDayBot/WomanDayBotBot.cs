2037d69d5ae925d0f4d10dc129d704c4a31c4e7f
BoikoAndrei1996 <boikoandrei1996@gmail.com>
2019-03-06 22:42:21 +0300

    hotfix

--- WomanDayBot/WomanDayBotBot.cs
+++ WomanDayBot/WomanDayBotBot.cs
@@ -110,6 +110,7 @@ namespace WomanDayBot
       CancellationToken cancellationToken)
     {
       var order = JsonConvert.DeserializeObject<Order>(value.ToString());
+      order.DocumentId = Guid.NewGuid();
       order.OrderId = Guid.NewGuid();
       order.RequestTime = DateTime.Now;
       order.UserData = userData;
